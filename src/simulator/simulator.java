9e4313405d64cc8e9245e3e29ad872b15d3bece2
the-kb <the-kb@ultronia2.0.2>
2020-08-12 23:04:53 +0200

    push

--- src/simulator/simulator.java
+++ src/simulator/simulator.java
@@ -1,4 +1,12 @@
 package simulator.aircrafts;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class simulator {
+    public static void main(String[] args){
+        readerObject = new BufferedReader(new FileReader("./scenario.txt"));
+        while(strLine = new readerObject.readLine() != null){
+            System.out.println(strLine);
+        }
+    }
 }

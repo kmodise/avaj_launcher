d6f5575c7ec3c66a83c43559738a2b53736dce5b
the-kb <the-kb@ultronia2.0.2>
2020-08-13 22:21:51 +0200

    Push

--- src/simulator/simulator.java
+++ src/simulator/simulator.java
@@ -1,12 +1,20 @@
 package simulator.aircrafts;
+import java.io.*;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.util.logging.Level;
+import java.util.logging.logger;
 
 public class simulator {
     public static void main(String[] args){
-        readerObject = new BufferedReader(new FileReader("./scenario.txt"));
-        while(strLine = new readerObject.readLine() != null){
-            System.out.println(strLine);
+        try {
+            // BufferedWriter bwObject = new BufferedWriter(new FileWriter("/home/the-kb/IdeaProjects/avaj_launcher/src/simulator/test.txt"));
+            // bwObject.write("This is the first line\nsecond line!!!");
+            BufferedReader brObject = new BufferedReader(new FileReader("/home/the-kb/IdeaProjects/avaj_launcher/src/simulator/scenario.txt"))
+            brObject.close();
+        } catch (IOException e) {
+
         }
-    }
+}
 }

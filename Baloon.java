692d545b932cbda43cbf6e20aff2bc1a641183e7
unknown <kmodise9898@gmail.com>
2020-09-06 13:43:20 +0200

    push

--- Baloon.java
+++ Baloon.java
@@ -3,22 +3,21 @@ import java.io.IOException;
 class Baloon extends Aircraft implements Flyable {
 	private WeatherTower weatherTower;
 
-	public Baloon(String name, Coordinates coordinates){super(name, coordinates);}
+	public Baloon(String name, Coordinates coordinates) {
+		super(name, coordinates);
+	}
 
-
-	private String craftID()
-	{
+	private String craftID() {
 		return ("Ballon#" + this.name + "(" + Long.toString(this.id) + "): ");
 	}
 
-	public void updateCondition(){
+	public void updateCondition() {
 		String weather = this.weatherTower.getWeather(this.coordinates);
 		int longitude = super.coordinates.getLongitude();
 		int latitude = super.coordinates.getLatitude();
 		int height = super.coordinates.getHeight();
-		
-		if (weather.equals("SUN"))
-		{
+
+		if (weather.equals("SUN")) {
 			try {
 				Simulation.writeToFile(craftID() + "SUN-> It's so hot this balloon might POP\n");
 			} catch (IOException e) {
@@ -27,31 +26,25 @@ class Baloon extends Aircraft implements Flyable {
 
 			longitude += 2;
 			height += 4;
-		}
-		else if (weather.equals("RAIN"))
-		{
+		} else if (weather.equals("RAIN")) {
 			try {
-				Simulation.writeToFile(craftID()+"RAIN-> is there an umbrella in this BALLOON?!!!!\n");
+				Simulation.writeToFile(craftID() + "RAIN-> is there an umbrella in this BALLOON?!!!!\n");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 
 			longitude += -5;
-		}
-		else if (weather.equals("FOG"))
-		{
+		} else if (weather.equals("FOG")) {
 			try {
-				Simulation.writeToFile(craftID()+"FOG-> FOG and BALLOONS don't mix!!!!!!!!!!!!\n");
+				Simulation.writeToFile(craftID() + "FOG-> FOG and BALLOONS don't mix!!!!!!!!!!!!\n");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 
 			longitude += -1;
-		}
-		else if (weather.equals("SNOW"))
-		{
+		} else if (weather.equals("SNOW")) {
 			try {
-				Simulation.writeToFile(craftID()+"SNOW-> Do you want to build a.... flying snowman?!!!\n");
+				Simulation.writeToFile(craftID() + "SNOW-> Do you want to build a.... flying snowman?!!!\n");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -60,8 +53,7 @@ class Baloon extends Aircraft implements Flyable {
 		}
 		if (height > 100)
 			height = 100;
-		if (height <= 0)
-		{
+		if (height <= 0) {
 			weatherTower.unregister(this);
 			try {
 				Simulation.writeToFile("\nBalloon#" + name + "(" + id + ") : Landing\n");
@@ -70,102 +62,15 @@ class Baloon extends Aircraft implements Flyable {
 			}
 		}
 		this.coordinates = new Coordinates(longitude, latitude, height);
-		
+
 	}
 
-	public void registerTower(WeatherTower weatherTower){
+	public void registerTower(WeatherTower weatherTower) {
 		this.weatherTower = weatherTower;
 		try {
-			Simulation.writeToFile("Balloon#"+name+"("+id+") : ");
+			Simulation.writeToFile("Balloon#" + name + "(" + id + ") : ");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-// 	public void updateCondition(){
-// 		String weather = this.weatherTower.getWeather(this.coordinates);
-// 		int longitude = 0;
-// 		int latitude = 0;
-// 		int height = 0;
-
-
-// 		try {
-// 			Simulation.writeToFile("Baloon#"+this.name+"("+this.id+") : ");
-// 		} catch (IOException e) {
-// 			e.printStackTrace();
-// 		}
-// 		if (weather.equals("SUN")) {
-// 			longitude = this.coordinates.getLongitude() + 2;
-// 			latitude = this.coordinates.getLatitude();
-// 			height = this.coordinates.getHeight() + 4;
-// 			try {
-// 				Simulation.writeToFile("It's so hot this balloon might POP\n");
-// 			} catch (IOException e) {
-// 				e.printStackTrace();
-// 			}
-// 		}
-// 		if (weather.equals("RAIN")) {
-
-// 			longitude = this.coordinates.getLongitude() - 5;
-// 			latitude = this.coordinates.getLatitude();
-// 			height = this.coordinates.getHeight();
-// 			try {
-// 				Simulation.writeToFile("is there an umbrella in this BALLOON?!!!!\n");
-// 			} catch (IOException e) {
-// 				e.printStackTrace();
-// 			}
-// 		}
-// 		if (weather.equals("FOG")) {
-// 			longitude = this.coordinates.getLongitude() - 1;
-// 			latitude = this.coordinates.getLatitude();
-// 			height = this.coordinates.getHeight();
-// 			try {
-// 				Simulation.writeToFile("FOG and BALLOONS don't mix!!!!!!!!!!!!\n");
-// 			} catch (IOException e) {
-// 				e.printStackTrace();
-// 			}
-// 		}
-// 		if (weather.equals("SNOW")) {
-// 			longitude = this.coordinates.getLongitude();
-// 			latitude = this.coordinates.getLatitude();
-// 			height = this.coordinates.getHeight() - 15;
-// 			try {
-// 				Simulation.writeToFile("It's so cold!!!!!!!!\n");
-// 			} catch (IOException e) {
-// 				e.printStackTrace();
-// 			}
-// 		}
-
-// 		if (height > 100)
-// 			height = 100;
-// 		if (height < 1){
-// 			try {
-// 				Simulation.writeToFile("\nBalloon#"+name+"("+id+") is Landing\n");
-// 			} catch (IOException e) {
-// 				e.printStackTrace();
-// 			}
-// 			this.weatherTower.unregister(this);
-// 			return ;}
-// 		this.coordinates = new Coordinates(longitude, latitude, height);
-// }
-
-// 	public void registerTower(WeatherTower weatherTower){
-// 		this.weatherTower = weatherTower;
-// 		try {
-// 			Simulation.writeToFile("Baloon#"+name+"("+id+") : ");
-// 		} catch (IOException e) {
-// 			e.printStackTrace();
-// 		}
-// 	}
 }
--- JetPlane.java
+++ JetPlane.java
@@ -4,54 +4,51 @@ import java.io.IOException;
 class JetPlane extends Aircraft implements Flyable {
 	private WeatherTower weatherTower;
 
-	public JetPlane(String name, Coordinates coordinates){super(name, coordinates);}
+	public JetPlane(String name, Coordinates coordinates) {
+		super(name, coordinates);
+	}
 
-	private String craftID()
-	{
+	private String craftID() {
 		return ("Jetplane#" + this.name + "(" + Long.toString(this.id) + "): ");
 	}
 
-	public void updateCondition(){
+	public void updateCondition() {
 		String weather = this.weatherTower.getWeather(this.coordinates);
 		int longitude = super.coordinates.getLongitude();
 		int latitude = super.coordinates.getLatitude();
 		int height = super.coordinates.getHeight();
-		
-		if (weather.equals("SUN"))
-		{
+
+		if (weather.equals("SUN")) {
 			try {
-				Simulation.writeToFile(craftID() + "SUN-> the SUN and her eyes were the only fire, my dead wife loved the sun\n");
+				Simulation.writeToFile(
+						craftID() + "SUN-> the SUN and her eyes were the only fire, my dead wife loved the sun\n");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 
 			longitude += 10;
 			height += 2;
-		}
-		else if (weather.equals("RAIN"))
-		{
+		} else if (weather.equals("RAIN")) {
 			try {
-				Simulation.writeToFile(craftID()+"RAIN-> Must be a leak in heaven, that's a lot of rain!\n");
+				Simulation.writeToFile(craftID() + "RAIN-> Must be a leak in heaven, that's a lot of rain!\n");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 
 			longitude += 5;
-		}
-		else if (weather.equals("FOG"))
-		{
+		} else if (weather.equals("FOG")) {
 			try {
-				Simulation.writeToFile(craftID()+"FOG-> Am I at a WWE event? it's so fogy, is THE UNDERTAKER 'bout to make his entrance?!!\n");
+				Simulation.writeToFile(craftID()
+						+ "FOG-> Am I at a WWE event? it's so fogy, is THE UNDERTAKER 'bout to make his entrance?!!\n");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 
 			longitude += 1;
-		}
-		else if (weather.equals("SNOW"))
-		{
+		} else if (weather.equals("SNOW")) {
 			try {
-				Simulation.writeToFile(craftID()+"SNOW-> all that is missing is a beautiful angel amongst all this SNOW\n");
+				Simulation.writeToFile(
+						craftID() + "SNOW-> all that is missing is a beautiful angel amongst all this SNOW\n");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -60,8 +57,7 @@ class JetPlane extends Aircraft implements Flyable {
 		}
 		if (height > 100)
 			height = 100;
-		if (height <= 0)
-		{
+		if (height <= 0) {
 			weatherTower.unregister(this);
 			try {
 				Simulation.writeToFile("\nHelicopter#" + name + "(" + id + ") : Landing\n");
@@ -70,14 +66,14 @@ class JetPlane extends Aircraft implements Flyable {
 			}
 		}
 		this.coordinates = new Coordinates(longitude, latitude, height);
-		
+
 	}
 
-	public void registerTower(WeatherTower weatherTower){
+	public void registerTower(WeatherTower weatherTower) {
 		this.weatherTower = weatherTower;
-		
+
 		try {
-			Simulation.writeToFile("JetPlane#"+name+"("+id+") : ");
+			Simulation.writeToFile("JetPlane#" + name + "(" + id + ") : ");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
